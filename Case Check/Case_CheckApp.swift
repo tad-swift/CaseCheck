7f1d6511cff0297a9747901b5639621236af03a4
Tadreik Campbell <tadreik@gmail.com>
2021-10-30 19:08:49 -0400

    u

--- Case Check/Case_CheckApp.swift
+++ Case Check/Case_CheckApp.swift
@@ -288,10 +288,10 @@ struct PhoneView: View {
     var phone: Phone
     var body: some View {
         HStack {
-            Image(uiImage: UIImage(named: phone.image)!)
+            Image(uiImage: UIImage(named: phone.image)?.trimmed() ?? UIImage())
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 70, height: 70, alignment: .center)
+                .frame(width: 70, height: 140, alignment: .center)
             Text("\(phone.name) cases")
                 .font(.system(size: 20, weight: .bold, design: .rounded))
                 .bold()
@@ -328,7 +328,7 @@ struct CasesView: View {
                 HStack(spacing: 20) {
                     ForEach(list, id: \.self) { item in
                         VStack {
-                            Image(uiImage: UIImage(named: item.image) ?? UIImage())
+                            Image(uiImage: UIImage(named: item.image)?.trimmed() ?? UIImage())
                                 .resizable()
                                 .aspectRatio(contentMode: .fit)
                                 .frame(width: 260, height: 260, alignment: .center)
@@ -352,36 +352,36 @@ struct CasesView: View {
             Spacer()
             if compatiblePhones.contains(phone.name) {
                 switch phone.name {
-                    case "iPhone 8":
-                        NavigationLink("View in 3D", destination: ModelView(sceneDirectory: "art.scnassets/iPhone 8.scn"))
-                            .font(.system(size: 16, weight: .medium, design: .rounded))
-                            .padding(.horizontal, 12)
-                            .padding(.vertical, 6)
-                            .background(RoundedRectangle(cornerRadius: 6).foregroundColor(Color.gray))
-                    case "iPhone X":
-                        NavigationLink("View in 3D", destination: ModelView(sceneDirectory: "art.scnassets/iPhoneX Master.scn"))
-                            .font(.system(size: 16, weight: .medium, design: .rounded))
-                            .padding(.horizontal, 12)
-                            .padding(.vertical, 6)
-                            .background(RoundedRectangle(cornerRadius: 6).foregroundColor(Color.gray))
-                    case "iPhone 11 Pro":
-                        NavigationLink("View in 3D", destination: ModelView(sceneDirectory: "art.scnassets/iPhone 11 Pro.scn"))
-                            .font(.system(size: 16, weight: .medium, design: .rounded))
-                            .padding(.horizontal, 12)
-                            .padding(.vertical, 6)
-                            .background(RoundedRectangle(cornerRadius: 6).foregroundColor(Color.gray))
-                    case "iPhone 12 Pro":
-                        NavigationLink("View in 3D", destination: ModelView(sceneDirectory: "art.scnassets/iPhone 12 Pro.scn"))
-                            .font(.system(size: 16, weight: .medium, design: .rounded))
-                            .padding(.horizontal, 12)
-                            .padding(.vertical, 6)
-                            .background(RoundedRectangle(cornerRadius: 6).foregroundColor(Color.gray))
-                    default:
-                        NavigationLink("View in 3D", destination: ModelView(sceneDirectory: "art.scnassets/iPhone 8.scn"))
-                            .font(.system(size: 16, weight: .medium, design: .rounded))
-                            .padding(.horizontal, 12)
-                            .padding(.vertical, 6)
-                            .background(RoundedRectangle(cornerRadius: 6).foregroundColor(Color.gray))
+                case "iPhone 8":
+                    NavigationLink("View in 3D", destination: ModelView(sceneDirectory: "art.scnassets/iPhone 8.scn"))
+                        .font(.system(size: 16, weight: .medium, design: .rounded))
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 6)
+                        .background(RoundedRectangle(cornerRadius: 6).foregroundColor(Color.gray))
+                case "iPhone X":
+                    NavigationLink("View in 3D", destination: ModelView(sceneDirectory: "art.scnassets/iPhoneX Master.scn"))
+                        .font(.system(size: 16, weight: .medium, design: .rounded))
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 6)
+                        .background(RoundedRectangle(cornerRadius: 6).foregroundColor(Color.gray))
+                case "iPhone 11 Pro":
+                    NavigationLink("View in 3D", destination: ModelView(sceneDirectory: "art.scnassets/iPhone 11 Pro.scn"))
+                        .font(.system(size: 16, weight: .medium, design: .rounded))
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 6)
+                        .background(RoundedRectangle(cornerRadius: 6).foregroundColor(Color.gray))
+                case "iPhone 12 Pro":
+                    NavigationLink("View in 3D", destination: ModelView(sceneDirectory: "art.scnassets/iPhone 12 Pro.scn"))
+                        .font(.system(size: 16, weight: .medium, design: .rounded))
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 6)
+                        .background(RoundedRectangle(cornerRadius: 6).foregroundColor(Color.gray))
+                default:
+                    NavigationLink("View in 3D", destination: ModelView(sceneDirectory: "art.scnassets/iPhone 8.scn"))
+                        .font(.system(size: 16, weight: .medium, design: .rounded))
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 6)
+                        .background(RoundedRectangle(cornerRadius: 6).foregroundColor(Color.gray))
                 }
             }
             Spacer()
@@ -439,3 +439,123 @@ extension Color {
     static var gray = Color(.sRGB, red: 0.95, green: 0.95, blue: 0.95, opacity: 1)
 }
 
+extension UIImage {
+
+    func trimmed() -> UIImage {
+        let newRect = self.cropRect
+        if let imageRef = self.cgImage!.cropping(to: newRect) {
+            return UIImage(cgImage: imageRef)
+        }
+        return self
+    }
+
+    var cropRect: CGRect {
+        let cgImage = self.cgImage
+        let context = createARGBBitmapContextFromImage(inImage: cgImage!)
+        if context == nil {
+            return CGRect.zero
+        }
+
+        let height = CGFloat(cgImage!.height)
+        let width = CGFloat(cgImage!.width)
+
+        let rect = CGRect(x: 0, y: 0, width: width, height: height)
+        context?.draw(cgImage!, in: rect)
+
+        //let data = UnsafePointer<CUnsignedChar>(CGBitmapContextGetData(context))
+        guard let data = context?.data?.assumingMemoryBound(to: UInt8.self) else {
+            return CGRect.zero
+        }
+
+        var lowX = width
+        var lowY = height
+        var highX: CGFloat = 0
+        var highY: CGFloat = 0
+
+        let heightInt = Int(height)
+        let widthInt = Int(width)
+        //Filter through data and look for non-transparent pixels.
+        for y in (0 ..< heightInt) {
+            let y = CGFloat(y)
+            for x in (0 ..< widthInt) {
+                let x = CGFloat(x)
+                let pixelIndex = (width * y + x) * 4 /* 4 for A, R, G, B */
+
+                if data[Int(pixelIndex)] == 0  { continue } // crop transparent
+
+                if data[Int(pixelIndex+1)] > 0xE0 && data[Int(pixelIndex+2)] > 0xE0 && data[Int(pixelIndex+3)] > 0xE0 { continue } // crop white
+
+                if (x < lowX) {
+                    lowX = x
+                }
+                if (x > highX) {
+                    highX = x
+                }
+                if (y < lowY) {
+                    lowY = y
+                }
+                if (y > highY) {
+                    highY = y
+                }
+
+            }
+        }
+
+        return CGRect(x: lowX, y: lowY, width: highX - lowX, height: highY - lowY)
+    }
+
+    func createARGBBitmapContextFromImage(inImage: CGImage) -> CGContext? {
+
+        let width = inImage.width
+        let height = inImage.height
+
+        let bitmapBytesPerRow = width * 4
+        let bitmapByteCount = bitmapBytesPerRow * height
+
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+
+        let bitmapData = malloc(bitmapByteCount)
+        if bitmapData == nil {
+            return nil
+        }
+
+        let context = CGContext (data: bitmapData,
+                                 width: width,
+                                 height: height,
+                                 bitsPerComponent: 8,      // bits per component
+            bytesPerRow: bitmapBytesPerRow,
+            space: colorSpace,
+            bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue)
+
+        return context
+    }
+    
+    func remove(color: UIColor) -> UIImage? {
+        
+        let cgColor = color.cgColor
+        let components = cgColor.components
+        var r = components?[0] ?? 0.0
+        var g = components?[1] ?? 0.0
+        var b = components?[2] ?? 0.0
+        
+        r = r * 255.0
+        g = g * 255.0
+        b = b * 255.0
+        
+        let colorMasking: [CGFloat] = [r, r, g, g, b, b]
+        
+        let image = UIImage(data: self.jpegData(compressionQuality: 1.0)!)!
+        let rawImageRef: CGImage = image.cgImage!
+        
+        UIGraphicsBeginImageContext(image.size);
+        
+        let maskedImageRef = rawImageRef.copy(maskingColorComponents: colorMasking)
+        UIGraphicsGetCurrentContext()?.translateBy(x: 0.0,y: image.size.height)
+        UIGraphicsGetCurrentContext()?.scaleBy(x: 1.0, y: -1.0)
+        UIGraphicsGetCurrentContext()?.draw(maskedImageRef!, in: CGRect.init(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        let result = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        return result
+    }
+}
